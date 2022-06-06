221289fcbd40ed7db99421081b37908f828d5361
H-123456 <106986840+H-123456@users.noreply.github.com>
2022-06-06 19:09:23 +0100

    Update PartD_Fork_1.py

--- PartD_Fork_1.py
+++ PartD_Fork_1.py
@@ -1,3 +1,14 @@
+# Aim to find a fork and analyse its effect
+
+# References: Fork (blockchain), Wikipedia, https://en.wikipedia.org/wiki/Fork_(blockchain) (accessed 15th April 2022)
+
+# Python Software Foundation, time – Time access and conversions, Python, 2001-2022, https://docs.python.org/3/library/time.html#time.strftime (accessed 15th April 2022)
+
+# Code also refers to some information/guidance from Big Data Science module
+
+
+
+
 #looking for when miners at same block at same day and what day happened
 
 from mrjob.job import MRJob
